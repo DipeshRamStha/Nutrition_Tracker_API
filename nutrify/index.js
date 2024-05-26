ec3979e0a9587e1d1a07e6fe954513c92182b9c4
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-26 08:59:02 -0500

    Created the token and sent the token with the login success message

--- nutrify/index.js
+++ nutrify/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// importing models
+const userModel = require("./models/userModel");
 // database connection
 mongoose
   .connect("mongodb://localhost:27017/nutrify")
@@ -14,6 +16,59 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
+// endpoint for registering user
+
+app.post("/register", (req, res) => {
+  let user = req.body;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (!err) {
+      bcrypt.hash(user.password, salt, async (err, hpass) => {
+        if (!err) {
+          user.password = hpass;
+          try {
+            let doc = await userModel.create(user);
+            res.status(201).send({ message: "User Registered" });
+          } catch (err) {
+            console.log(err);
+            res.status(500).send({ message: "Some Problem" });
+          }
+        }
+      });
+    }
+  });
+});
+
+// end point for login
+
+app.post("/login", async (req, res) => {
+  let userCred = req.body;
+
+  try {
+    const user = await userModel.findOne({ email: userCred.email });
+    if (user !== null) {
+      bcrypt.compare(userCred.password, user.password, (err, success) => {
+        if (success == true) {
+          jwt.sign({ email: userCred.email }, "nutrifyapp", (err, token) => {
+            if (!err) {
+              res.send({ message: "Login Success", token: token });
+            }
+          });
+        } else {
+          res.status(403).send({ message: "Incorrect password" });
+        }
+      });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Some Problem" });
+  }
+});
 
 app.listen(8000, () => {
   console.log("Server is up and running");
